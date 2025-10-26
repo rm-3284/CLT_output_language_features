0b555ee9e5dbb33caf62d2023d1c4a5242824a09
rm-3284 <rm4411@princeton.edu>
2025-10-25 20:34:56 -0400

    cuda device

--- src/device_setup.py
+++ src/device_setup.py
@@ -2,7 +2,7 @@ import torch
 from transformer_lens.utils import get_device
 
 device = get_device()
-if device == 'cuda':
+if torch.cuda.is_available():
     num_gpus = torch.cuda.device_count()
 
 from huggingface_hub import login
