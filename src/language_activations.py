b1a9045f9a34f7e8b9f70015daac2d52ede0c6ac
rm-3284 <rm4411@princeton.edu>
2025-10-29 01:24:40 -0400

    no squeeze

--- src/language_activations.py
+++ src/language_activations.py
@@ -90,8 +90,8 @@ def sae_features_from_activations(
             
             K = 32
             top_values, top_indices_batch = torch.topk(feature_acts, k=K, dim=-1)
-        top_acts.append(top_values.unsqueeze(0).cpu())
-        top_indices.append(top_indices_batch.unsqueeze(0).cpu())
+        top_acts.append(top_values.cpu())
+        top_indices.append(top_indices_batch.cpu())
     
     top_acts = torch.cat(top_acts, dim=1)
     top_acts = torch.split(top_acts, activations_size, dim=1)
