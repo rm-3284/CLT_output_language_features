a587c40019ea9011be16094f89d6510136f5394b
rm-3284 <rm4411@princeton.edu>
2025-10-29 10:05:26 -0400

    debug

--- src/language_activations.py
+++ src/language_activations.py
@@ -87,7 +87,7 @@ def sae_features_from_activations(
         print(chunk.shape, input_activation.shape)
         with torch.no_grad():
             # (batch, seq_len, d_sae)
-            _, feature_acts, *args = sae(input_activation)
+            feature_acts = sae(input_activation)
             print(feature_acts.shape)
             K = 100
             top_values, top_indices_batch = torch.topk(feature_acts, k=K, dim=-1)
