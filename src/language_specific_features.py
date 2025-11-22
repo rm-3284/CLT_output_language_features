0f4ba0a3ff3144abfd89d675eb24c93efd7386eb
rm-3284 <rm4411@princeton.edu>
2025-11-22 13:15:54 -0500

    bos token manual fix

--- src/language_specific_features.py
+++ src/language_specific_features.py
@@ -142,14 +142,10 @@ def scale_steer_to_A(
 
     generated = prompt
     for _ in range(max_new_tokens):
-        encoded_input = model.tokenizer(
-                prompt,
-                return_tensors="pt",
-                add_special_tokens=True,
-                # This ensures the BOS token is added
-            ).input_ids
+        if generated == '':
+            generated = torch.tensor(2) # bos token
         graph = attribute(
-                prompt=encoded_input,
+                prompt=generated,
                 model=model,
                 max_n_logits=max_n_logits,
                 desired_logit_prob=desired_logit_prob,
