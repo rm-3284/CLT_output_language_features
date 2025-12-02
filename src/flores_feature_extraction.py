c738079abf863f0f7cc40b0c1759ea826172491b
rm-3284 <rm4411@princeton.edu>
2025-12-02 15:07:17 -0500

    revert

--- src/flores_feature_extraction.py
+++ src/flores_feature_extraction.py
@@ -21,9 +21,8 @@ def iterate_through_sentences(
         max_n_logits = 5, desired_logit_prob = 0.95,
         max_feature_nodes = None, batch_size = 256,
         offload = 'cpu', verbose = True,
-        ) -> tuple[list[tuple[int, int]], dict[str, list[float]]]:
+        ) -> list[tuple[int, int]]:
     features = []
-    activation_dict = dict()
     for prompt in sentences:
         graph = attribute(
             prompt=prompt,
@@ -35,9 +34,6 @@ def iterate_through_sentences(
             offload=offload,
             verbose=verbose,
         )
-        active_features = graph.active_features
-        activation_values = graph.activation_values
-        n_pos = graph.n_pos
         paths = []
         decoded = model.tokenizer.encode(prompt)
         for pos in range (1, len(decoded)): # the first one is <bos>
@@ -54,24 +50,8 @@ def iterate_through_sentences(
         pruned = prune_paths_by_first_last(graph, paths, threshold_first, threshold_last)
         last_pos_features = pick_last_pos_features(graph, pruned)
         features.extend(last_pos_features)
-
-        pos = n_pos - 1
-        for layer, feature_idx in last_pos_features:
-            target_row = torch.tensor((layer, pos, feature_idx))
-            matches = (active_features == target_row)
-            row_matches_all = torch.all(matches, dim=1)
-            indices = torch.nonzero(row_matches_all, as_tuple=False)
-            activation_val = 0
-            if indices.numel() > 0:
-                index = indices.item()
-                activation_val = activation_values[index].detach().cpu()
-            key = f"{layer}.{feature_idx}"
-            try:
-                activation_dict[key].append(activation_val)
-            except KeyError:
-                activation_dict = [activation_val]
     
-    return features, activation_dict
+    return features
 
 if __name__ == "__main__":
     current_file_path = __file__
@@ -93,11 +73,7 @@ if __name__ == "__main__":
         df = ds.to_pandas()
         batch = df.loc[:150, 'text'].tolist()
         sentences = filter_sentences(batch, alphabet_char[lang], model) # only returns 100 sentences
-        features, activations = iterate_through_sentences(model, sentences)
+        features = iterate_through_sentences(model, sentences)
         file_name = f'{lang}.json'
         with open(os.path.join(data_directory, file_name), 'w') as f:
             json.dump(features, f)
-        file_name = f'{lang}_activations.json'
-        with open(os.path.join(data_directory, file_name), 'w') as f:
-            json.dump(activations, f)
-
